caf7b584321ecfea9c2267e7c41bdec53a4a32c0
ajcytrap <pmm.gwo@gmail.com>
2023-06-09 09:35:29 +0200

    latest version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,9 @@ import java.util.stream.Collectors;
 public class Main {
 
 
+
     public static void main(String[] args) {
+
         ArrayList<String> bricksInBox = new ArrayList<>();
         ArrayList<String> instructions = new ArrayList<>();
         int unusedBricks = 0;
@@ -21,29 +23,54 @@ public class Main {
             System.out.println("klpops");
             return;
         }
+
         String path = args[0];
         readFile(path, bricksInBox, instructions, unusedBricks);
 
     }
 
-    private static void readFile(String path, ArrayList<String> bricksInBox, ArrayList<String> instructions, int unusedBricks) {
-        try (FileReader fileReader = new FileReader(path); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line = bufferedReader.readLine();
-            String[] parts = line.split(":");
-            int number = Integer.parseInt(parts[0]);
-            line = parts[1];
-            line = line.replaceAll("[\\\\r\\\\n]", "");
-            if (isValid(line)) {
-                while (line != null) {
-                    filterNotUsed(line, unusedBricks);
-                    putBricksInBox(line, bricksInBox);
-                    addToInstructions(line, instructions);
-                }
+//    private static void readFile(String path, ArrayList<String> bricksInBox, ArrayList<String> instructions, int unusedBricks) {
+//        try (FileReader fileReader = new FileReader(path); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+//            String line = bufferedReader.readLine();
+//            String[] parts = line.split(":");
+//            int number = Integer.parseInt(parts[0]);
+//            line = parts[1];
+//            line = line.replaceAll("[\\\\r\\\\n]", "");
+//            if (isValid(line)) {
+//                while (line != null) {
+//                    filterNotUsed(line, unusedBricks);
+//                    putBricksInBox(line, bricksInBox);
+//                    addToInstructions(line, instructions);
+//                    line = bufferedReader.readLine();
+//                }
+//            }
+//        } catch (IOException exception) {
+//            System.out.println("klopps");
+//        }
+//    }
+private static void readFile(String path, ArrayList<String> bricksInBox, ArrayList<String> instructions, int unusedBricks) {
+    try (FileReader fileReader = new FileReader(path); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+        String line;
+        int lineCount = 0;
+        while ((line = bufferedReader.readLine()) != null) {
+            lineCount++;
+            if (lineCount > 15000000) {
+                System.out.println("Limit of 15,000,000 lines exceeded.");
+                break;
             }
-        } catch (IOException exception) {
-            System.out.println("klopps");
+            line = line.trim();
+            if (isValid(line)) {
+                filterNotUsed(line, unusedBricks);
+                putBricksInBox(line, bricksInBox);
+                addToInstructions(line, instructions);
+            } else {
+                System.out.println("Invalid line: " + line);
+            }
         }
+    } catch (IOException exception) {
+        System.out.println("Error reading the file: " + exception.getMessage());
     }
+}
 
 
     private static void putBricksInBox(String line, ArrayList<String> bricksInBox) {
@@ -63,19 +90,17 @@ public class Main {
     }
 
     private static boolean isValid(String line) {
-        String regex = "^\\d+:[(A-O)]{4}$";
+        String regex = "^\\d+:[A-O]{4}$";
         int maxLines = 150_000_000;
         int lineCount = 0;
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(line);
-        if (!line.isEmpty() && isValid(line)) {
-            lineCount++;
-            if (lineCount >= maxLines) {
-                System.out.println("klopis");
-            }
-
+        if (matcher.matches()) {
+            return true;
+        } else {
+            System.out.println("Invalid line: " + line);
+            return false;
         }
-        return matcher.matches();
     }
 
     private static void removeBricksFromBox(List<String> instructionList, ArrayList<String> bricksInBox) {
